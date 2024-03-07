1cff103869e5a08a94f056268e402e14de7dfd13
Your Name <you@example.com>
2024-03-07 23:28:41 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navbar />
+    <Main />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Main from './components/Task.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Main,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar navbar-light bg-light p-3">
+        <h2 class="navbar-brand ">
+            <img src="https://stellarblue.eu/assets/img/logo.png" class="logo" alt="">
+        </h2>
+    </nav>
+</template>
+
+
+<script>
+    export default{
+        name: 'Navbar'
+    }
+</script>
